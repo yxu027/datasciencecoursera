9e8c8230afb3721e5a6ecbdfef35dee410ec069f
yxu027 <yxu027@ucr.edu>
2020-09-19 15:09:41 -0700

    add comments

--- R language week2 project.R
+++ R language week2 project.R
@@ -2,11 +2,11 @@ setwd("X:/Chrome Download")
 
   
 pollutantmean <- function(directory, pollutant, id = 1:332){
-  a=c()
+  a=c() #to save for loop result as a vector, need a exist vector outside the loop enviroment
   for(i in id){
     path <- paste(getwd(), "/", directory, "/", sprintf("%03d", i), ".csv", sep = "")
     data <- read.csv(path)
-    a <- c(a, data[,pollutant])
+    a <- c(a, data[,pollutant]) #save for loop result in a vector, otherwise will be overwrite ever loop
   }
   mean(a, na.rm = TRUE)
   
